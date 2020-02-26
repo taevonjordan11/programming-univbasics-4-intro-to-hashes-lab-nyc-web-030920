e45565ad6221005924cb2cb8369bfe346b9576ed
Taevon Jordan <taevonjordan1@gmail.com>
2020-02-26 01:20:23 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -21,9 +21,7 @@ def my_hash_creator(key, value)
 end
 
 def read_from_hash(hash, key)
-  hash= {:key1 => key}
-  hash[:key]
-end
+  hash[key]
 
 def update_counting_hash(hash, key)
   # given a hash an a key as parameters, return an updated hash
