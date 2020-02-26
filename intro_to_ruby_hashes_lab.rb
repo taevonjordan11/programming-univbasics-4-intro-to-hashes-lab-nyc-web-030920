a97bf73439bffcb689fd0e2e4e70e289ebd36bb1
Taevon Jordan <taevonjordan1@gmail.com>
2020-02-26 00:52:14 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -15,9 +15,7 @@ def id_generator
 end
 
 def my_hash_creator(key, value)
-  binding.pry
- #taevon_hash= {}
-	#taevon_hash[:name] = "Grace Hopper"
+  hash_two = {:key => value}
 end
 
 def read_from_hash(hash, key)
